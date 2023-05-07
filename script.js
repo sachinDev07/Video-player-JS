93ef6c95899ca453989c94ac651ec663101988b8
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-05-08 00:19:19 +0500

    duration mode and playback mode is done

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const fullScreenBtn = document.querySelector(".full_screen_btn");
 const minPlayerBtn = document.querySelector(".mini_player_btn");
 const muteBtn = document.querySelector(".mute_btn");
 const volumeslider = document.querySelector(".volume_slider");
+const presentTime = document.querySelector(".current_time");
+const totalTime = document.querySelector(".total_time");
+const speedBtn = document.querySelector(".speed_btn");
 
 document.addEventListener("keydown", (e) => {
   const tagName = document.activeElement.tagName.toLowerCase();
@@ -30,16 +33,63 @@ document.addEventListener("keydown", (e) => {
     case "m":
       toggleMute();
       break;
+    case "arrowleft":
+    case "j":
+      skip(-5);
+      break;
+    case "arrowright":
+    case "l":
+      skip(5);
+      break;
   }
 });
+
+// Playback Mode
+speedBtn.addEventListener("click", changePlaybackSpeed);
+
+function changePlaybackSpeed() {
+  let newPlaybackRate = video.playbackRate + 0.25;
+  console.log(newPlaybackRate);
+  if (newPlaybackRate > 2) newPlaybackRate = 0.25;
+  video.playbackRate = newPlaybackRate;
+  speedBtn.textContent = `${newPlaybackRate}x`;
+}
+
+// Duration Mode
+video.addEventListener("loadeddata", () => {
+  totalTime.textContent = formatDuration(video.duration);
+});
+
+video.addEventListener("timeupdate", () => {
+  presentTime.textContent = formatDuration(video.currentTime);
+});
+
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2,
+});
+
+function formatDuration(time) {
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+
+  return hours === 0
+    ? `${minutes}:${leadingZeroFormatter.format(seconds)}`
+    : `${hours}:${leadingZeroFormatter.format(minutes)}:
+       ${leadingZeroFormatter.format(seconds)}`;
+}
+
+function skip(duration) {
+  video.currentTime += duration;
+}
 
 // Volume Modes
 muteBtn.addEventListener("click", toggleMute);
 
-volumeslider.addEventListener("input", e => {
+volumeslider.addEventListener("input", (e) => {
   video.volume = e.target.value;
   video.muted = e.target.value === 0;
-})
+});
 
 function toggleMute() {
   video.muted = !video.muted;
@@ -48,19 +98,17 @@ function toggleMute() {
 video.addEventListener("volumechange", () => {
   volumeslider.value = video.volume;
   let volumeLevel;
-  if(video.muted || video.volume === 0) {
+  if (video.muted || video.volume === 0) {
     volumeslider.value = 0;
     volumeLevel = "muted";
-  }else if(video.volume >= 0.5) {
-    volumeLevel = "high"
-  }else {
-    volumeLevel = "low"
+  } else if (video.volume >= 0.5) {
+    volumeLevel = "high";
+  } else {
+    volumeLevel = "low";
   }
 
   container.dataset.volumeLevel = volumeLevel;
-})
-
-
+});
 
 // View Modes
 theaterBtn.addEventListener("click", toggleTheaterMode);
@@ -98,7 +146,6 @@ video.addEventListener("enterpictureinpicture", () => {
 video.addEventListener("leavepictureinpicture", () => {
   container.classList.remove("mini_player");
 });
-
 
 // Play/pause
 playPauseButton.addEventListener("click", togglePlay);
